de18443cc1de3102c00a090ce0ee07d1f8f67a38
Frederick Essono <fredericktabi30@gmail.com>
2023-03-14 16:05:37 +0100

    users wip

---
+++ Routes/Users.py
@@ -0,0 +1,22 @@
+from typing import Dict, List, Optional
+from fastapi import Depends, FastAPI, HTTPException, Request, Response, status, APIRouter
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.templating import Jinja2Templates
+from Security.Controllers import LoginController
+from Security.Settings import Settings
+from Security.DTO.User import User
+
+
+
+route = APIRouter(prefix='')
+templates = Jinja2Templates(directory="templates")
+
+
+@route.get("/users", response_class=HTMLResponse)
+def index(request: Request, user: User = Depends(LoginController.get_current_user_from_token)):
+    context = {
+        "user": user,
+        "request": request
+    }
+    return templates.TemplateResponse("users.html", context)
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from Database import Models
 from Database.Connexion import engine
 import uvicorn
 from Security.Routes.Login_register import Login_register
+from Routes import Users
 
 Models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(Login_register.route) 
+app.include_router(Users.route) 
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=9000)
