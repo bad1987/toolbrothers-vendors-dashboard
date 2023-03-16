d8dc9e021ebba757d3fa7f2cc9bacc139dad4967
Mafo312 <mafobruno990@gmail.com>
2023-03-16 16:15:39 +0100

    Updating

--- App/Http/Controllers/OrderController.py
+++ App/Http/Controllers/OrderController.py
@@ -5,6 +5,7 @@ from Database.CscartModels import CscartOrders
 
 class OrderController:
     def get_orders_by_vendor_connected(request: Request, db_local: Session, db_cscart: Session):
+        
         user = LoginController.get_current_user_from_cookie(request, db_local)
         orders = db_cscart.query(CscartOrders).filter(CscartOrders.company_id == user.company_id).all()
         
