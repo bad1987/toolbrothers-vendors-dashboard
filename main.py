85989f665ffe21c8146e5e4f0729ba255e7af20a
bayanga antoine <badinfo1987@gmail.com>
2023-03-22 12:50:04 +0100

    configuring cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Database.Connexion import engine
 import uvicorn
 from Security.Routes.Login_register import Login_register
 from Routes import Users, Orders, Errors, Payments
+from fastapi.middleware.cors import CORSMiddleware
 
 from middlewares.FirewallMiddleware import firewall_middleware
 from middlewares.AuthorizationMiddleware import orders_permissions
@@ -14,6 +15,10 @@ from fastapi.staticfiles import StaticFiles
 # Models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173"
+]
+
 app.include_router(Login_register.route) 
 app.include_router(Users.route) 
 app.include_router(Orders.route)
@@ -23,6 +28,14 @@ app.include_router(Payments.route)
 # static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # authorizations
 app.add_middleware(BaseHTTPMiddleware, dispatch=orders_permissions)
 # allow only authenticated users
