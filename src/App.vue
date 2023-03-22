54fc61306d502307d8566c565039bc0f8bca584f
bayanga antoine <badinfo1987@gmail.com>
2023-03-22 11:09:46 +0100

    removing duplicated classes

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@ import FooterComponent from './components/FooterComponent.vue';
 
 <template>
     <Navbar></Navbar>
-    <div class="dark:bg-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:border-gray-700">
-        <div class="flex overflow-hidden bg-white pt-16 dark:bg-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:border-gray-700">
+    <div class="dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex overflow-hidden bg-white pt-16 dark:bg-gray-800 dark:border-gray-700">
             <Sidebar></Sidebar>
             <div class="bg-gray-900 opacity-50 hidden fixed inset-0 z-10 dark:bg-gray-800 dark:border-gray-700" id="sidebarBackdrop"></div>
             <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64 dark:bg-gray-800 dark:border-gray-700">
-                <div class="main-section min-h-screen dark:bg-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:border-gray-700">
+                <div class="main-section min-h-screen dark:bg-gray-800 dark:border-gray-700">
                     <RouterView></RouterView>
                 </div>
                 <FooterComponent></FooterComponent>
