a4a6c1188b7ab5d8fa76afa897731f14e1f62b84
Mafo312 <mafobruno990@gmail.com>
2023-03-24 09:59:24 +0100

    Updating

--- Database/Models.py
+++ Database/Models.py
@@ -46,23 +46,15 @@ class Platform_settings(Base):
     __tablename__ = "platform_settings"
     
     id = Column(Integer, primary_key=True, index=True)
-    platform_id = Column(String(100), index=True, nullable=True)
-    referrer_id = Column(String(100), index=True, nullable=True)
-    api_id = Column(String(100), index=True, nullable=True)
-    api_secret = Column(String(100), index=True, nullable=True)
-    access_token = Column(String(100), index=True, nullable=True)
+    platform_id = Column(Integer, index=True, nullable=True)
+    referrer_id = Column(Integer, index=True, nullable=True)
+    api_id = Column(Integer, index=True, nullable=True)
+    api_secret = Column(Integer, index=True, nullable=True)
+    access_token = Column(Integer, index=True, nullable=True)
     platform_url = Column(String(250), index=True, nullable=True)
-    name = Column(String(255), unique=True, index=True, nullable=False)
-    processor_id = Column(Integer, nullable=False)
+    method_payment_id = Column(Integer, index=True, nullable=True)
+    shipping_profile_id = Column(Integer, index=True, nullable=True)
     
     # payment_method_vendor = relationship("Payment_method_vendor",  back_populates="payment_method", cascade="all, delete")
-    # Plenty ID:
-    # Referrer ID:
-    # API ID:
-    # API SECRET:
-    # ACCESS TOKEN:
-    # Plenty Url:
-    # Method of payment ID:
-    # Shipping profile ID: VAT:
-    
+
     
