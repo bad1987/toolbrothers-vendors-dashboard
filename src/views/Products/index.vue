9220cde14a9d0ec995b1832f7e96dd5059e4598b
Frederick Essono <fredericktabi30@gmail.com>
2023-04-28 10:42:25 +0100

    manual change on products

--- src/views/Products/index.vue
+++ src/views/Products/index.vue
@@ -60,6 +60,7 @@ function updateProduct(obj = null) {
   isLoading.value = true
   axios.put("/products/" + selectedProduct.value.product_id, obj == null ? selectedProduct.value : obj)
         .then(response => {
+          console.log(response.data)
           document.getElementById("edit-product-modal")?.click()
           let ans = products.value.map(x => x.product_id === selectedProduct.value.product_id ? response.data : x)
           products.value = ans
@@ -74,6 +75,17 @@ function deactivateProduct(id) {
   Object.assign(selectedProduct.value, products.value.find(x => x.product_id == id))
 
   updateProduct({ status: selectedProduct.value.status == 'A' ? 'D' : 'A', product_id: selectedProduct.value.product_id })
+}
+
+function changeManually(id) {
+  Object.assign(selectedProduct.value, products.value.find(x => x.product_id == id))
+
+  if (selectedProduct.value.manual_change == null) {
+    isLoading.value = false
+    return
+  }
+
+  updateProduct({ manual_change: selectedProduct.value.manual_change == true ? false : true, product_id: id })
 }
 
 function nextPage() {
@@ -182,7 +194,15 @@ fetchProducts()
       </div>
       <!-- Table -->
       <div class="flex flex-col mt-6 relative">
-        <div v-if="isLoading" class="absolute top-0 left-0 w-full h-full bg-white opacity-50"></div>
+        <div v-if="isLoading" class="absolute top-0 left-0 w-full h-full bg-white opacity-50 z-50 flex">
+          <div role="status" class="w-max m-auto">
+              <svg aria-hidden="true" class="inline w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+              </svg>
+              <span class="sr-only">Loading...</span>
+          </div>
+        </div>
       <div class="overflow-x-auto rounded-lg">
         <div class="inline-block min-w-full align-middle">
           <div class="overflow-hidden shadow sm:rounded-lg">
@@ -209,6 +229,9 @@ fetchProducts()
                     </th>
                     <th scope="col" class="p-4 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-white">
                       Action
+                    </th>
+                    <th scope="col" class="p-4 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-white">
+                      Manual change
                     </th>
                   </tr>
                 </thead>
@@ -275,6 +298,12 @@ fetchProducts()
                             </svg>
                             Edit
                         </button>
+                      </td>
+                      <td class="">
+                        <label @click="changeManually(product.product_id)" class="relative inline-flex items-center cursor-pointer">
+                          <input v-model="product.manual_change" type="checkbox" value="" class="sr-only peer">
+                          <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        </label>
                       </td>
                     </tr>
                 </tbody>
