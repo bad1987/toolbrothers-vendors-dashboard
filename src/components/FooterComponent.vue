f519d8661be6e7d5a8b263306b0103a484c10fce
Frederick Essono <fredericktabi30@gmail.com>
2023-05-04 09:48:02 +0100

    footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,8 +1,21 @@
-<template>
+<!-- <template>
 
 <footer class="fixed bottom-0 left-0 md:w-2/3 text-center justify-center items-center justify-items-center z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Dinotech™</a>. All Rights Reserved.
     </span>
 </footer>
+
+</template> -->
+
+<template>
+
+    <footer class="bg-white dark:bg-gray-900">
+        <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 m-auto">© 2023 <a href="https://toolbrothers.com/" class="hover:underline">Dinotech™</a>. All Rights Reserved.
+            </span>
+        </div>
+        </div>
+    </footer>
 
 </template>
