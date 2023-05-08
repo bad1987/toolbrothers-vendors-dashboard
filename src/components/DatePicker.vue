52ae8bf1175aaa49504b889c7a6e728567e30eec
Frederick Essono <fredericktabi30@gmail.com>
2023-05-08 09:18:48 +0100

    remove autocomplete on input fields

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -42,6 +42,7 @@ function handleSubmitDate() {
             </svg>
         </div>
         <input type="text" name="start_date" required
+            autocomplete="off"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             placeholder="Start date"> 
         </div>
@@ -58,6 +59,7 @@ function handleSubmitDate() {
             </svg>
         </div>
         <input type="text" name="end_date"  required
+            autocomplete="off"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             placeholder="End date">
         </div>
