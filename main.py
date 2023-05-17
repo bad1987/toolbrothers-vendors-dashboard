803b538ebe04b051df749a6e287dd360c7c1110e
Frederick Essono <fredericktabi30@gmail.com>
2023-05-17 14:07:46 +0100

    commit

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi.staticfiles import StaticFiles
 
-from App.input_ports.routes.api import order_routes
+from App.input_ports.routes.api import order_routes, product_routes
 from App.input_ports.routes.system.settings import Payment_route
-from App.input_ports.routes.system import user_routes
-from App.input_ports.routes.api import product_routes
+from App.input_ports.routes.system import s_product_routes, user_routes, vendor_routes
 
 from starlette.middleware.base import BaseHTTPMiddleware
 
@@ -32,6 +31,8 @@ app.include_router(product_routes.api_route)
 
 app.include_router(Payment_route.route)
 app.include_router(user_routes.s_user_route)
+app.include_router(vendor_routes.route)
+app.include_router(s_product_routes.route)
  
 # static files
 # app.mount("/static", StaticFiles(directory="static"), name="static")
