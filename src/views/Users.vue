ad50c1b4738210a04632a31b488b1aa8923165cc
Frederick Essono remote <fredericktabi30@gmail.com>
2023-06-20 14:44:51 +0000

    wip

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -176,6 +176,20 @@ const newUser = ref({
   roles: null,
   permissions: null,
 });
+
+// Delete user
+const deleteUser = (user_id) => {
+  axios
+    .delete("/admin/user/" + user_id)
+    .then((res) => {
+      alert.value.showAlert("success", "User deleted successfully!!", "Successful!!");
+      fetchUsers();
+    })
+    .catch((error) => {
+      alert.value.showAlert("error", "Operation fail please retry !!", "error!!");
+    });
+};
+
 </script>
 
 <template>
@@ -521,6 +535,28 @@ const newUser = ref({
                         ></path>
                       </svg>
                       Activate
+                    </button>
+                    <button
+                      @click="deleteUser(u.id)"
+                      type="button"
+                      class="inline-flex items-center px-3 py-1 text-sm font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-600 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
+                    >
+                      <svg
+                        class="w-4 h-4 mr-2"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                        ></path>
+                      </svg>
+                      Delete user
                     </button>
                   </td>
                 </tr>
