60bc968d1cadbaa3fe25d8a61c1031a52045a667
Frederick Essono remote <fredericktabi30@gmail.com>
2023-07-04 16:56:04 +0000

    changes from server

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ app.include_router(user_platform_routes.route)
 # cors
 app.add_middleware( 
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
